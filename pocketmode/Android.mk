32487d19127becf9f71365f7756d402d2a8a6fea
cyproto <kyashkarande@gmail.com>
2018-11-01 09:47:31 +0000

    adjust doze for carbon and other changes

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -1,3 +1,6 @@
+
+
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -16,9 +19,6 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-preference \
     android-support-v7-recyclerview \
     android-support-v14-preference
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
