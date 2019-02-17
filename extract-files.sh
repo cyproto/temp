LineageOS/android_device_leeco_msm8996-common

msm8996-common: Hax libaudcal.so to store acdbdata in new path
Change-Id: I83984260c843f526b9b0db99f21dc44ab78e4735

extract-files.sh
@@ -73,4 +73,12 @@ COMMON_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
 sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" \
     "$COMMON_BLOB_ROOT"/vendor/etc/permissions/qti_libpermissions.xml
 
+#
+# Hax libaudcal.so to store acdbdata in new path
+#
+sed -i "s|\/data\/vendor\/misc\/audio\/acdbdata\/delta\/|\/data\/vendor\/audio\/acdbdata\/delta\/\x00\x00\x00\x00\x00|g" \
+    "$COMMON_BLOB_ROOT"/vendor/lib/libaudcal.so
+sed -i "s|\/data\/vendor\/misc\/audio\/acdbdata\/delta\/|\/data\/vendor\/audio\/acdbdata\/delta\/\x00\x00\x00\x00\x00|g" \
+    "$COMMON_BLOB_ROOT"/vendor/lib64/libaudcal.so
+
 "$MY_DIR"/setup-makefiles.sh

cyproto/temp

Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
Change-Id: Ib8a826b51ab4b28356cb04e03bb894f267e9bdf7

extract-files.sh
@@ -72,4 +72,12 @@ sed -i "s|libgui.so|libfui.so|g" "$BLOB_ROOT"/vendor/lib/libmmcamera2_stats_modu
 patchelf --remove-needed libandroid.so "$BLOB_ROOT"/vendor/lib/libmmcamera2_stats_modules.so
 patchelf --remove-needed libandroid.so "$BLOB_ROOT"/vendor/lib/libmpbase.so
 
+#
+# Hax libaudcal.so to store acdbdata in new path
+#
+sed -i "s|\/data\/vendor\/misc\/audio\/acdbdata\/delta\/|\/data\/vendor\/audio\/acdbdata\/delta\/\x00\x00\x00\x00\x00|g" \
+    "$BLOB_ROOT"/vendor/lib/libaudcal.so
+sed -i "s|\/data\/vendor\/misc\/audio\/acdbdata\/delta\/|\/data\/vendor\/audio\/acdbdata\/delta\/\x00\x00\x00\x00\x00|g" \
+    "$BLOB_ROOT"/vendor/lib64/libaudcal.so
+
 "$MY_DIR"/setup-makefiles.sh